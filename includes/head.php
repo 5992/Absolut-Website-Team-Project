b4492b551eae8c35cf714b7f3d29cfa4e83741a7
Yen Duong <5992@ait.nsw.edu.au>
2018-05-15 06:13:02 +0000

    thuongto - update database files and product list loaded

--- includes/head.php
+++ includes/head.php
@@ -6,4 +6,10 @@
   <script src="components/jquery/dist/jquery.js"></script>
   <script src="components/bootstrap/dist/js/bootstrap.js"></script>
   <script src="script.js"></script>
+  
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
---
+++ includes/navbar.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Absolut. Vodka</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Product</a></li>
+      <li><a href="#">About</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+<!--<nav class="navbar navbar-inverse navbar-static-top">-->
+<!--        <div class="container-fluid">-->
+<!--        <div class="navbar-header">-->
+<!--          <a class="navbar-brand">-->
+<!--            Absolut.-->
+<!--          </a>-->
+<!--          <button class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">-->
+<!--            <span class="icon-bar"></span>-->
+<!--            <span class="icon-bar"></span>-->
+<!--            <span class="icon-bar"></span>-->
+<!--          </button>-->
+<!--        </div>-->
+<!--        <div class="collapse navbar-collapse" id="main-nav">-->
+<!--          <ul class="nav navbar-nav navbar-right">-->
+<!--            <li class="active"><a href="index.php">Home</a></li>-->
+<!--            <li><a href="about.php">About</a></li>-->
+<!--            <li><a href="product.php">Product</a></li>-->
+<!--          </ul>-->
+<!--        </div>-->
+<!--        </div>-->
+<!--      </nav>-->
